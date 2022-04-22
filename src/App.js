b98fc549b66599fc2f4545aba21c6d9a2d916bb6
sandeepsg1 <sandeepsg686@gmail.com>
2022-04-22 12:47:29 +0530

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddInventory from './components/classComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Product Inventory form by Bootstrap</h1>
+        <AddInventory/>
+      
       </header>
     </div>
   );
